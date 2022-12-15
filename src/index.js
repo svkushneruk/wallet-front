ad2e0463d621ce9d39044466fa1524a795c7e112
Serhii Kushneruk <svkushneruk@ukr.net>
2022-12-15 19:26:11 +0200

    remote basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/https://svkushneruk.github.io/wallet-front/home/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
